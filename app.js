04877a7d6611dea24ae810d6be5f46ae626959ab
IvanMiyasoied <121316974+IvanMiyasoied@users.noreply.github.com>
2023-05-30 00:40:12 +0300

    first part

---
+++ app.js
@@ -0,0 +1,40 @@
+// let obj = new Object ();
+// obj.name = 'John';
+// obj.age = 35;
+// console.log(obj);
+
+// obj.dayBirth = '10.05.1988'
+// console.log(obj.dayBirth);
+
+
+// function Car(model,year,speed) {
+//     this.model = model;
+//     this.year = year;
+//     this.speed = speed;
+//     this.getSpeedUp = getSpeedUp;
+// }
+
+// let myCar = new Car('superb',2019,240);
+
+
+
+// function getSpeedUp(myCar) {
+//         this.speed = this.speed + 10
+//     }
+
+    
+// myCar.speedUp = getSpeedUp;
+
+// myCar.speedUp(myCar);
+// console.log(myCar.speed);
+
+
+let customer = {
+    name : 'Karl',
+    adress: 'Sydney',
+    order : {
+        good_art : 'microwave',
+        quantity : 10
+    }
+
+}
