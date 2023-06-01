f8d82655618c606f3ee313f2da5af2fdb18d5833
IvanMiyasoied <121316974+IvanMiyasoied@users.noreply.github.com>
2023-06-02 01:07:13 +0300

    done

--- app.js
+++ app.js
@@ -29,12 +29,140 @@
 // console.log(myCar.speed);
 
 
-let customer = {
-    name : 'Karl',
-    adress: 'Sydney',
-    order : {
-        good_art : 'microwave',
-        quantity : 10
-    }
+// let customer = {
+//     name : 'Karl',
+//     adress: 'Sydney',
+//     order : {
+//         good_art : 'microwave',
+//         quantity : 10
+//     }
 
-}
+// }
+
+
+// function createObj() {
+//     let user = {}
+//     user.name = 'Ivan';
+//     user.surname = 'Smith';
+//     user.name = 'Petro'
+//     delete user.name
+//     return user
+// }
+
+// createObj();
+// ncosole.log(createObj());
+
+
+// function isEmpty (obj) {
+//     let emptyObj = !Object.keys(obj).length;
+//     return emptyObj
+// }
+
+// isEmpty({
+//     John : 100,
+//     Ann : 160,
+//     Pete : 130,
+// });
+// console.log(isEmpty({
+//     John : 100,
+//     Ann : 160,
+//     Pete : 130,
+// }));
+
+
+// function sum(obj) {
+//      let result = 0;
+//      for (key in obj) {
+//         result += obj[key];
+//      }
+//      return result
+// }
+
+// sum({
+//     John : 100,
+//     Ann : 160,
+//     Pete : 130,
+// });
+
+// console.log(sum({
+//     John : 100,
+//     Ann : 160,
+//     Pete : 130,
+// }));
+
+
+
+// function multiplyNum (obj) {
+//      Object.keys(obj).map(i => obj[i] *= 2)
+     
+//      return obj
+// }
+
+// multiplyNum({
+//     John : 100,
+//     Ann : 160,
+//     Pete : 130,
+// });
+
+// console.log(multiplyNum({
+//     John : 100,
+//     Ann : 160,
+//     Pete : 130,
+// }));
+
+
+// let calculator = {
+//     sum: function() {
+//       return this.a + this.b;
+//     },
+  
+//     mul: function() {
+//       return this.a * this.b;
+//     },
+  
+//     readValues: function() {
+//       this.a = +prompt('a?', 0);
+//       this.b = +prompt('b?', 0);
+//     }
+//   }
+  
+//   calculator.readValues();
+//   alert(calculator.sum());
+//   alert(calculator.mul());
+
+
+// let ladder = {
+//     step: 0,
+//      up() {
+//         this.step++;
+//         return this
+//     },
+//      down() {
+//         this.step--;
+//         return this
+//     },
+//     showStep: function () {
+//         alert(this.step);
+//         return this
+//     }
+// };
+
+// ladder.up().up().down().showStep().down().showStep()
+
+
+// function addFullName (user) {
+//     user.fullName = user.name + ' ' + user.surname
+//     return user
+// }
+
+// addFullName ({
+//     name: 'Ivan',
+//     surname:'Vasylchenko'
+// });
+
+// console.log(addFullName ({
+//     name: 'Ivan',
+//     surname:'Vasylchenko'
+// }));
+
+
